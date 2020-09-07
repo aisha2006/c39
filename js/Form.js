81ac61aeea317d69e96adac95d87e072facde7a3
aisha <aishapannali779@gmail.com>
2020-09-07 11:57:56 +0530

    marking the player

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton("reset");
   }
   hide(){
     this.greeting.hide();
@@ -19,7 +20,8 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-
+    this.reset.position(displayWidth-100,20);
+    
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
@@ -27,10 +29,16 @@ class Form {
       playerCount+=1;
       player.index = playerCount;
       player.update();
+      //Greeting for player 4
       player.updateCount(playerCount);
-      this.greeting.html("Hello " + player.name)
+      this.greeting.html("Hello " + player.name);
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
 
+    this.reset.mousePressed(()=>{
+      game.update(0);
+      player.updateCount(0);
+    });
+    
   }
 }
--- js/Game.js
+++ js/Game.js
@@ -77,7 +77,10 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          
+          //cars[index - 1].shapeColor = "red";
+          fill("red");
+          ellipse(x,y,80,80);
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
         }
